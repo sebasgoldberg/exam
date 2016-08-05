8eed25136ab2ab3c9d41788cad0fb1a8693a802d
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2016-08-05 18:11:12 -0300

    Begining get_test_note implementation.

--- examapp/models.py
+++ examapp/models.py
@@ -5,6 +5,17 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from random import randint
+
+
+class ExamException(Exception):
+    pass
+
+
+class QuestionsQuantityError(ExamException):
+    pass
+
+
 class Manual(models.Model):
     
     manual = models.CharField(max_length=256, verbose_name=_(u'Manual'))
@@ -15,7 +26,7 @@ class Manual(models.Model):
         app_label = 'examapp'
 
     def __unicode__(self):
-        return u'[%s] %s' % (self.manual, self.subject)
+        return u'%s' % self.manual
 
 
 class Subject(models.Model):
@@ -35,7 +46,7 @@ class Subject(models.Model):
 class Question(models.Model):
     
     subject = models.ForeignKey(Subject, verbose_name=_(u'Subject'))
-    page = models.PositiveSmallIntegerField(verbose_name=_(u'Page'))
+    page = models.PositiveSmallIntegerField(verbose_name=_(u'Page'), blank=True, null=True)
     question = models.TextField(verbose_name=_(u'Question'))
 
     class Meta:
@@ -74,19 +85,47 @@ class TestModel(models.Model):
     questions = models.PositiveSmallIntegerField(verbose_name=_(u'Number of questions'))
 
     class Meta:
-        verbose_name = _(u"Test Model Subject")
-        verbose_name_plural = _(u"Test Model Subjects")
+        verbose_name = _(u"Test Model")
+        verbose_name_plural = _(u"Test Models")
         app_label = 'examapp'
 
     def __unicode__(self):
-        return u'%s [%s]' % (self.subject, self.weight)
+        return u'%s' % self.test_model
 
+    def random_weights(self):
+        total_weight = 0
+        tms_weights = []
+        for tms in self.testmodelsubject_set.all():
+            weight = float(tms.random_weight())
+            tms_weights.append((tms, weight,))
+            total_weight = total_weight + tms.random_weight() 
+        return (tms_weights, total_weight)
+
+    def create_test(self):
+        t=Test.objects.create(test_model=self)
+
+        tms_weights, total_weight = self.random_weights()
+
+        total_questions = 0
+        for tms, weight in tms_weights[:-1]:
+            questions = self.questions * (
+                weight / total_weight )
+            total_questions = total_questions + len(create_random_test_questions(tms, questions))
+        
+        tms, weight = tms_weights[-1]
+        total_questions = total_questions + len(t.create_random_test_questions(tms, self.questions - total_questions))
+
+        if total_questions <> self.questions:
+            raise QuestionsQuantityError()
+
+        return t
 
 class TestModelSubject(models.Model):
 
     test_model = models.ForeignKey(TestModel, verbose_name=_(u'Test Model'))
     subject = models.ForeignKey(Subject, verbose_name=_(u'Subject'))
-    weight = models.PositiveSmallIntegerField(verbose_name=_(u'Weight'))
+    weight_from = models.PositiveSmallIntegerField(verbose_name=_(u'Weight from'))
+    weight_to = models.PositiveSmallIntegerField(verbose_name=_(u'Weight to'))
 
     class Meta:
         verbose_name = _(u"Test Model Subject")
@@ -94,6 +133,47 @@ class TestModelSubject(models.Model):
         app_label = 'examapp'
 
     def __unicode__(self):
-        return u'%s [%s]' % (self.subject, self.weight)
+        return u'%s [%s-%s]' % (self.subject, self.weight_from, self.weight_to)
 
+    def random_weight(self):
+       return randint(self.weight_from, self.weight_to) 
 
+    def random_questions(self, n):
+        for q in self.subject.question_set.order_by('?')[:n]:
+            yield q
+
+class Test(models.Model):
+    test_model = models.ForeignKey(TestModel, verbose_name=_(u'Test Model'))
+
+    def create_random_test_questions(self, tms, n):
+        result = []
+        for q in tms.random_questions(n):
+            tq = self.testquestion_set.create(
+                ref_question=q,
+                test=self)
+
+            tq.create_test_answers()
+
+            result.append(tq)
+
+        return result
+
+    def get_test_note(self):
+        self.testquestions
+        for tq in self.test
+
+class TestQuestion(models.Model):
+    ref_question = models.ForeignKey(Question, verbose_name=_(u'Question'))
+    test = models.ForeignKey(Test, verbose_name=_(u'Test'))
+
+    def create_test_answers(self):
+        for a in self.ref_question.answer_set.all():
+            self.testanswer_set.create(
+                ref_answer = a,
+                test_question = self
+            )
+
+class TestAnswer(models.Model):
+    ref_answer = models.ForeignKey(Answer, verbose_name=_(u'Answer'))
+    test_question = models.ForeignKey(TestQuestion, verbose_name=_(u'Test Question'))
+    is_correct = models.BooleanField(verbose_name=_(u'Is Correct?'), default=False)
--- examapp/tests.py
+++ examapp/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
+from examapp.models import *
 
-# Create your tests here.
+class TestExam(TestCase):
+
+    def setUp(self):
+
+        self.manual = Manual.objects.create()
+        self.subject = Subject.objects.create(manual=self.manual)
+
+        self.Q1 = Question.objects.create(
+            subject=self.subject,
+            question=u'Q1')
+
+        self.Q1A1 = self.Q1.answer_set.create(
+            answer=u'A1',
+            is_correct=True
+            )
+
+        self.Q1A2 = Answer.objects.create(
+            question=self.Q1,
+            answer=u'A2',
+            is_correct=False
+            )
+
+        self.TM = TestModel.objects.create(questions=1)
+
+        self.TM.testmodelsubject_set.create(
+            subject=self.subject,
+            weight_from=1,
+            weight_to=1)
+
+    def test_create_random_test_questions(self):
+
+        T = self.TM.create_test()
+
+        questions = T.create_random_test_questions(
+            self.subject.testmodelsubject_set.first(),1)
+
+        self.assertEqual(len(questions),1)
+
+    def test_random_weights(self):
+
+        tms_weights, total_weight = self.TM.random_weights()
+
+        self.assertEqual(total_weight,1)
+        self.assertEqual(len(tms_weights),1)
+
+    def test_exam_test(self):
+
+        T = self.TM.create_test()
+
+        self.assertEqual(T.testquestion_set.count(), 1)
+
+        TQ1 = T.testquestion_set.all()[0]
+
+        self.assertEqual(TQ1.testanswer_set.count(), 2)
+
+        TQ1A1 = TQ1.testanswer_set.get(ref_answer__answer='A1')
+        TQ1A2 = TQ1.testanswer_set.get(ref_answer__answer='A2')
+
+        TQ1A1.is_correct = True
+        TQ1A1.save()
+
+        TQ1A2.is_correct = False
+        TQ1A2.save()
+        
+        note = T.get_test_note()
+
+        self.assertEqual(note, 1)
